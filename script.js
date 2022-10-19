2fc94e894abf4c86a1752acbbc37428050f8b17b
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-10-19 06:57:09 -0500

    blur effect

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ password.addEventListener('input', (e) => {
   const length = input.length
   const blurValue = 20 - length * 2
 
-  console.log(blurValue)
+  background.style.filter = `blur(${blurValue}px)`
   
 })
